9be54db9d3924aefa42356209958fa71dce4d1b0
Gaurav Gupta <gaurav@randomsync.net>
2020-01-11 23:41:24 -0800

    Initial command based program with DifferentialDrive using TalonSRXs

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class DriveConstants {
+        public static final int FRONT_LEFT_MOTOR_ID = 6;
+        public static final int FRONT_RIGHT_MOTOR_ID = 7;
+        public static final int REAR_LEFT_MOTOR_ID = 2;
+        public static final int REAR_RIGHT_MOTOR_ID = 4;
+    }
+
+    public static final class OIConstants {
+        public static final int XBOX_CONTROLLER = 0;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase {
+
+  private final SpeedControllerGroup leftControllerGroup = new SpeedControllerGroup(
+      new WPI_TalonSRX(DriveConstants.FRONT_LEFT_MOTOR_ID), new WPI_TalonSRX(DriveConstants.REAR_LEFT_MOTOR_ID));
+
+  private final SpeedControllerGroup rightControllerGroup = new SpeedControllerGroup(
+      new WPI_TalonSRX(DriveConstants.FRONT_RIGHT_MOTOR_ID), new WPI_TalonSRX(DriveConstants.REAR_RIGHT_MOTOR_ID));
+
+  private final DifferentialDrive drive = new DifferentialDrive(leftControllerGroup, rightControllerGroup);
+
+  public DriveSubsystem() {
+
+  }
+
+  public void arcadeDrive(double xSpeed, double zRotation) {
+    drive.arcadeDrive(xSpeed, zRotation);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
