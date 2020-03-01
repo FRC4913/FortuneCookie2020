7b5aa334c94917d0ef88419912cdaa0471cdfbf8
Gaurav Gupta <ggupta@kindsys.us>
2020-03-01 15:17:40 -0800

    Update color rotator motor to use VictorSPX controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,9 +32,9 @@ public final class Constants {
         public static final int XBOX_CONTROLLER = 0;
     }
 
-    public static final int COLOR_PANEL_ROTATOR_MOTOR_ID = 0;
     public static final I2C.Port I2C_PORT = I2C.Port.kOnboard;
     public static final int LOADER_MOTOR = 1;
     public static final int INTAKER_MOTOR = 5;
     public static final int SHOOTER_MOTOR = 3;
+    public static final int COLOR_ROTATOR_MOTOR = 10;
 }
