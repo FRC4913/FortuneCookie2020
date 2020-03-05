4a3aa552e29cb3a13fbe9001c62c8de133cf2688
Team 4913 <frc4913@fairmontschools.com>
2020-03-05 15:45:41 -0800

    rasberry pi connector

--- src/main/java/frc/robot/subsystems/RasberryPiConnector.java
+++ src/main/java/frc/robot/subsystems/RasberryPiConnector.java
@@ -6,7 +6,23 @@ import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+//for network table
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class RasberryPiConnector extends SubsystemBase{
-    
+    NetworkTableInstance networkInstance = NetworkTableInstance.getDefault();
+
+    public RasberryPiConnector() {
+        //table
+        NetworkTable RasberryPiTable = networkInstance.getTable("RasberryPidatatable");
+        //entry
+        NetworkTableEntry xEntry = RasberryPiTable.getEntry("X");
+
+        SmartDashboard.putString("datatable test variable: " ,  xEntry.getString("Error"));
+    }
+
 }
 
